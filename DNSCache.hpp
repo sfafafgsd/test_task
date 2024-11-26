c914cea0964c05128f664f7006399f468d4876d8
sfafafgsd <161938576+sfafafgsd@users.noreply.github.com>
2024-11-26 16:55:27 +0300

    refactoring

--- DNSCache.hpp
+++ DNSCache.hpp
@@ -4,35 +4,39 @@
 #include <string>
 #include <unordered_map>
 #include <mutex>
+#include <memory>
 
 struct DNSCacheNode 
 {
-	std::string name;
-	std::string ip;
-	DNSCacheNode* next;
-	DNSCacheNode* prev;
+	std::string_view name = "";
+	std::string_view ip = "";
+	DNSCacheNode* next = nullptr;
+	DNSCacheNode* prev = nullptr;
 
 	DNSCacheNode();
-	DNSCacheNode(const std::string& _name, const std::string& _ip);
+	DNSCacheNode(const std::string_view& _name, const std::string_view& _ip);
 };
 
 class DNSCache
 {
 private:
 	size_t m_size;
-	std::unordered_map<std::string, DNSCacheNode*> m_cache;
-	DNSCacheNode* m_head;
-	DNSCacheNode* m_tail;
+	std::unordered_map<std::string_view, DNSCacheNode*> m_cache;
+
+	std::unique_ptr<DNSCacheNode[]> m_allocated_nodes;
+	size_t m_node_counter = 0;
+
+	DNSCacheNode* m_head = nullptr;
+	DNSCacheNode* m_tail = nullptr;
 
 	std::mutex m_mutex;
 
-	void delete_node(DNSCacheNode* node);
+	void remove_node(DNSCacheNode* node);
 	void move2head(DNSCacheNode* node);
 public:
 	explicit DNSCache(size_t max_size);
-	void update(const std::string& name, const std::string& ip);
-	std::string resolve(const std::string& name);
-	~DNSCache();
+	void update(const std::string_view& name, const std::string_view& ip);
+	std::string_view resolve(const std::string_view& name);
 };
 
 #endif
